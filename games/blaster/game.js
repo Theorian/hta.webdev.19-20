c0363fc6beb66115fb2e614d1243df7c09897aca
Underdoglegend1319 <54081796+Underdoglegend1319@users.noreply.github.com>
2019-11-15 17:47:35 -1000

    Update game.js

--- games/blaster/game.js
+++ games/blaster/game.js
@@ -39,27 +39,12 @@ var lastFired = 10;
 
 function preload() {
   this.load.image("Ship", "assets/HeroShip.png");
-  this.load.image("Bullet", "assets/Ammunition.png");
+  this.load.image("Bullet", "assets/Hero Ammunition.png");
   this.load.image("background", "assets/Background.png");
-  this.load.image("bullet", "assets/EnemyshipAmmunition.png");
+  this.load.image("bullet", "assets/Enemy ship Ammunition (1).png");
   this.load.image("Dead", "assets/HeroDeath.png");
   this.load.image("badship", "assets/enemy.png");
   this.load.image("dead", "assets/EnemyshipDeath.png");
-}
-
-this.anims.create({
-    key: "jump",
-    frames: this.anims.generateFrameNumbers("Ship", { start: 0, end: 6 }),
-    frameRate: 14,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: "idle",
-    frames: this.anims.generateFrameNumbers("Ship", { start: 1, end: 1 }),
-    frameRate: 1,
-    repeat: -1
-  });
 }
 
 function create() {
