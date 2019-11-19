acecd3f251ff5908a068801756a655e74f5d9ead
ac@theorian.com <ac@theorian.com>
2019-11-18 16:27:57 -1000

    Updated space blaster and some other items

--- games/blaster/game.js
+++ games/blaster/game.js
@@ -9,161 +9,30 @@
 //        |_|      
 // 
 
-window.onload = function() {
-  // object containing configuration options
-  let gameConfig = {
-    type: Phaser.WEBGL,
-    backgroundColor: "#ffffff",
-    pixelArt: true,
-    scale: {
-      parent: 'gamediv',
-      mode: Phaser.Scale.RESIZE,
-      autoCenter: Phaser.Scale.CENTER_BOTH,
-      zoom: 1,
-      width: 640,
-      height: 480
-    },
-    physics: {
-      default: "arcade",
-      arcade: {
-        gravity: { y: 0 },
-        debug: false
-      }
-    },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    }
-  };
-  game = new Phaser.Game(gameConfig);
-  window.focus();
 
+var config = {
+  type: Phaser.WEBGL,
+  backgroundColor: "black",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    zoom: 1,
+    width: 640,
+    height: 480
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  scene: [
+    SceneMainMenu,
+    SceneMain,
+    SceneGameOver
+  ],
+  pixelArt: true,
+  // roundPixels: true
 };
 
-var Bullet;
-var Ship;
-var speed;
-var stats;
-var cursors;
-var lastFired = 10;
-var spaceTileMap;
-
-function preload() {
-  this.load.image("bullet", "assets/HeroAmmunition.png");
-  this.load.image("background", "assets/bg.gif");
-  this.load.image("bad", "assets/enemy.png");
-  this.load.image("enemyBullet", "assets/EnemyAmmunition.png");
-  this.load.image("dead", "assets/EnemyshipDeath.png");
-
-    // player is a sprite sheet made by 24x48 pixels
-    this.load.spritesheet("ship", "assets/HeroShip.png", {
-      frameWidth: 100,
-      frameHeight: 100
-    });
-}
-
-function create() {
-
-  //Create the Starfield background
-  spaceTileMap = this.add.tileSprite(1000, 1000, 2000, 2000, 'background');
-  spaceTileMap.setTileScale(5,5);
-  //Create the bullets from this class
-  var Bullet = new Phaser.Class({
-    Extends: Phaser.GameObjects.Image,
-
-    initialize: function Bullet(scene) {
-      Phaser.GameObjects.Image.call(this, scene, 0, 0, "bullet");
-
-      this.speed = Phaser.Math.GetSpeed(700, 1);
-    },
-
-    fire: function(x, y) {
-      this.setPosition(x, y - 40);
-
-      this.setActive(true);
-      this.setVisible(true);
-    },
-
-    update: function(time, delta) {
-      this.y -= this.speed * delta;
-
-      if (this.y < -50) {
-        this.setActive(false);
-        this.setVisible(false);
-      }
-    }
-  });
-
-  Bullets = this.add.group({
-    classType: Bullet,
-    maxSize: 20,
-    runChildUpdate: true
-  });
-
-  Ship = this.physics.add.sprite(400, 500, "ship").setDepth(1);
-
-  Ship.body.setCollideWorldBounds();
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  speed = Phaser.Math.GetSpeed(500, 1);
-
-  this.anims.create({
-    key: "shipRockets",
-    frames: this.anims.generateFrameNumbers("ship", {
-      start: 0,
-      end: 4
-    }),
-    frameRate: 8,
-    repeat: -1
-  });
-
-  Ship.anims.play("shipRockets");
-
-}
-
-function update(time, delta) {
-
-  
-  spaceTileMap.tilePositionY -= 3; 
-  
-  if (cursors.left.isDown) {
-    Ship.x -= speed * delta;
-    spaceTileMap.tilePositionX -= 1; 
-  }
-  if (cursors.right.isDown) {
-    Ship.x += speed * delta;
-    spaceTileMap.tilePositionX += 1; 
-  }
-  if (cursors.up.isDown) {
-    Ship.y -= speed * delta;
-  }
-  if (cursors.down.isDown) {
-    Ship.y += speed * delta;
-  }
-
-  if (cursors.space.isDown && time > lastFired) {
-    var Bullet = Bullets.get();
-
-    if (Bullet) {
-      Bullet.fire(Ship.x, Ship.y);
-
-      lastFired = time + 50;
-    }
-  }
-
-  // if (cursors.up.isDown && time > lastFired)
-  // {
-  //     var bullet = bullets.get();
-
-  //     if (bullet)
-  //     {
-  //         bullet.fire(ship.x, ship.y);
-
-  //         lastFired = time + 50;
-  //     }
-  // }
-}
-
-
+var game = new Phaser.Game(config);
--- games/runner/assets/clouds.tsx
+++ games/runner/assets/clouds.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="clouds" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="247" columns="19">
- <image source="clouds.png" trans="000080" width="325" height="231"/>
+<tileset version="1.2" tiledversion="1.3.0" name="clouds" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="9" columns="3">
+ <image source="clouds.png" trans="000080" width="52" height="52"/>
 </tileset>
